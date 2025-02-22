f6688417edea76b0c4048ca3ba63c7daf17826d8
fabioadler <adlerluna@hotmail.com>
2025-02-22 13:07:17 -0300

    update navbar

--- navbar/app.py
+++ navbar/app.py
@@ -3,10 +3,11 @@ from flet import *
 def my_app(page:Page):
     page.title = "v1 nav_bar"
     page.navigation_bar = NavigationBar(
+        selected_index=1,
         destinations=[
-            NavigationBarDestination(icon=Icons.HOME, label="Home"),
             NavigationBarDestination(icon=Icons.ADD_LOCATION, label="Locais"),
-            NavigationBarDestination(icon=Icons.MENU, label="Configuração"),
+            NavigationBarDestination(icon=Icons.HOME, label="Home"),
+            NavigationBarDestination(icon=Icons.MENU, label="Configuração")
         ]
     )
     page.update()
