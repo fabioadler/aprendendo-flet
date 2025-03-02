b59d9e892c9171b21cd04e1689b4cc8e55c1769d
Fábio Adler de Luna Gomes <56208895+fabioadler@users.noreply.github.com>
2025-03-01 23:09:38 -0300

    Responsividade da tela (teste)

--- tela_login_riot/app.py
+++ tela_login_riot/app.py
@@ -1,54 +1,55 @@
-import flet as ft
+import flet as ft, time
 
 class my_app():
     def __init__(self,page:ft.Page):
         self.page = page
         self.page.title = "Login RIOT"
         self.page.window.title_bar_hidden = True
-        self.page.window.resizable = False
-        self.page.window.width = 1200
-        self.page.window.height = 600
+        self.page.window.resizable = True
         self.page.window.left = 20
-        self.page.padding = 0
+        self.page.on_resized = self.update()
 
+    def home(self):
         self.stack = ft.Stack(
             expand = True,
             controls=[
                 ft.Container(
                     expand = True,
                     content=ft.Row(
-                        spacing=0,
-                        controls=[
+                        spacing = 0,
+                        controls = [
                             ft.Container(
-                                width = 300,
-                                height = 600,
-                                padding=0,
-                                gradient=ft.LinearGradient(
-                                    begin=ft.alignment.top_center,
-                                    end=ft.alignment.bottom_center,
-                                    colors=["#09121D","#162437","#152943","#193457"]
+                                width = (0.25*self.page.window.width),
+                                height = (1*self.page.window.height),
+                                padding = 0,
+                                gradient = ft.LinearGradient(
+                                    begin = ft.alignment.top_center,
+                                    end = ft.alignment.bottom_center,
+                                    colors = ["#09121D","#162437","#152943","#193457"]
                                 ),
-                                content=ft.Column(
+                                content = ft.Column(
                                     horizontal_alignment = "center",
-                                    controls=[
+                                    controls = [
                                         ft.Container(
-                                            content=ft.Image(
+                                            content = ft.Image(
                                                 src = "riot-games-new-logo-qhg.png",
-                                                width = 200,
-                                                height = 200,
-                                                fit=ft.ImageFit.COVER
+                                                width = (0.7*self.page.window.width),
+                                                height = (0.2*self.page.window.height),
+                                                fit = ft.ImageFit.COVER
                                             )
                                         )
                                     ]
                                 )
                             ),
                             ft.Container(
-                                padding=0,
-                                content=ft.Image(
+                                width = (0.75*self.page.window.width),
+                                height = (1*self.page.window.height),
+                                padding = 0,
+                                content = ft.Image(
                                     src = "background.jpg",
-                                    width = 900,
-                                    height = 600,
-                                    fit=ft.ImageFit.COVER
+                                    width = (0.75*self.page.window.width),
+                                    height = (1*self.page.window.height),
+                                    fit = ft.ImageFit.COVER
                                 )
                                 
                             )
@@ -56,31 +57,41 @@ class my_app():
                     )
                 ),
                 ft.Row(
+                    width=(1*self.page.window.width),
                     controls=[
                         ft.WindowDragArea(
-                            width=self.page.window.width - 120,
-                            height=30,
-                            content=ft.Container(bgcolor="transparent")
+                            width = (0.9*self.page.window.width),
+                            height = 30,
+                            content = ft.Container(bgcolor="transparent")
                         ),
                         ft.IconButton(
-                            icon=ft.Icons.REMOVE,
-                            icon_color=ft.Colors.WHITE,
-                            on_click=self.mini
+                            icon = ft.Icons.REMOVE,
+                            icon_color = ft.Colors.WHITE,
+                            on_click = self.mini
                         ),
                         ft.IconButton(
-                            icon=ft.Icons.CLOSE,
-                            icon_color=ft.Colors.WHITE,
-                            on_click=lambda e: self.page.window.close()
+                            icon = ft.Icons.CLOSE,
+                            icon_color = ft.Colors.WHITE,
+                            on_click = lambda e: self.page.window.close()
                         )
                     ]
                 )
             ]
         )
         self.page.add(self.stack)
-        self.page.update()
+        
 
     def mini(self, e):
         self.page.window.minimized = True
         self.page.update()
+
+    def update(self):
+        self.page.clean()
+        self.home()
+        self.page.padding=0
+        self.page.update()
+        time.sleep(1)
+        self.page.on_resized = self.update()
+
 
 ft.app(target=my_app,assets_dir="./assets")
