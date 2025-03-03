96ff2cc81c46c89835afebe37987c19115808b48
Fábio Adler de Luna Gomes <56208895+fabioadler@users.noreply.github.com>
2025-03-03 16:03:54 -0300

    Adicionando lib que faltava

--- carrosel/app.py
+++ carrosel/app.py
@@ -2,6 +2,10 @@ import flet as ft
 from fletcarousel import BasicAnimatedHorizontalCarousel as bc, HintLine, AutoCycle
 
 def my_app(page:ft.Page):
-    pass
+    page.window.width = 720
+    page.window.height = 1024
+    page.padding = 0
+    page.title = "Carrosel teste 01"
+    page.update()
 
 ft.app(target=my_app, assets_dir="./assets")
